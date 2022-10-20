9c43cf0281d2f67e78b8066b3d21467204352863
GranSly <bob031299@gmail.com>
2022-10-21 02:02:06 +1000

    add stylelint eslint prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js"
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg"
+import styles from "./App.module.css"
 
 const App: Component = () => {
   return (
@@ -9,7 +9,7 @@ const App: Component = () => {
       <header class={styles.header}>
         <img src={logo} class={styles.logo} alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edait <code>src/App.tsx</code> and save to reload. asdasd
         </p>
         <a
           class={styles.link}
@@ -21,7 +21,7 @@ const App: Component = () => {
         </a>
       </header>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
